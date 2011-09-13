741e4cb72d30d2cdeff195efa3ea1e2cd7ae4b96
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-13 17:25:20 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facecontrol::Application.config.secret_token = '5ed95c83f2b92b6023ac9ac4ff361e198b10f82b8eead2afb3be46ff1e957a4a6d2598b5bcc09b82ff8da143ccb862963baeaa266c258fa9349466e316e91a8f'
