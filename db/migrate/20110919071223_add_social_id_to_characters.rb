e6bfde17dedf7dd18c99e5dafb4890916919eebd
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-19 11:15:25 +0400

    fix social_id migration

--- db/migrate/20110919071223_add_social_id_to_characters.rb
+++ db/migrate/20110919071223_add_social_id_to_characters.rb
@@ -1,7 +1,6 @@
 class AddSocialIdToCharacters < ActiveRecord::Migration
   def change
     add_column :characters, :social_id, :string
+    add_index :characters, :social_id
   end
-
-  add_index :characters, :social_id
 end
