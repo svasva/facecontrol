3827a7fd9dca345f891b066fe0948a0de1e7bba4
duke <duke@duke-vaio.(none)>
2011-09-19 18:47:57 +0400

    add names to default actions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class PlacesController < ApplicationController
   
   before_filter :authenticate_admin!
--- app/models/place.rb
+++ app/models/place.rb
@@ -14,7 +14,7 @@ class Place < ActiveRecord::Base
   	:class_name => 'Action',
 		:conditions => {:default_type => "stay"},
 		:autosave => true
-		
+
   has_one :leave_action,
   	:as => :subject,
   	:class_name => 'Action',
@@ -24,6 +24,7 @@ class Place < ActiveRecord::Base
   accepts_nested_attributes_for :enter_action, :stay_action, :leave_action, :actions
 
   after_initialize :init_default_actions
+  before_create :add_names_to_default_actions
 
 
   private #studio
@@ -35,6 +36,12 @@ class Place < ActiveRecord::Base
 			build_leave_action
 			enter_action.conditions.build	
 		end
+  end
+
+  def add_names_to_default_actions
+		enter_action.name = "Войти в #{name}"
+		stay_action.name = "Находится в #{name}"
+		leave_action.name = "Выйти из #{name}"
   end
 
   include Models::Place::CsvExchange
