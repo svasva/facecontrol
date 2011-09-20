b8d2d688bb5297e5c3724c2da9171dbbf86e8e7b
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-20 23:57:01 +0400

    Item: refactor scopes and default actions

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,34 +1,33 @@
 class Item < ActiveRecord::Base
-  has_many :actions, :as => :subject
-  has_one :buy_action,
-		:as => :subject,
-		:class_name => 'Action',
-		:conditions => {:default_type => "buy"},
-		:autosave => true
+  has_many :actions, :as => :subject, :dependent => :destroy
+  has_one :buy_for_money_action,
+    :as => :subject,
+    :class_name => 'Action',
+    :conditions => {:default_type => "buy_for_money"},
+    :autosave => true
 
-  has_one :wear_action,
-		:as => :subject,
-		:class_name => 'Action',
-		:conditions => {:default_type => "wear"},
-		:autosave => true
-
-  has_one :take_off_action,
-		:as => :subject,
-		:class_name => 'Action',
-		:conditions => {:default_type => "take_off"},
-		:autosave => true
+  has_one :buy_for_energy_action,
+    :as => :subject,
+    :class_name => 'Action',
+    :conditions => {:default_type => "buy_for_energy"},
+    :autosave => true
 
   has_one :gift_action,
-		:as => :subject,
-		:class_name => 'Action',
-		:conditions => {:default_type => "gift"},
-		:autosave => true
+    :as => :subject,
+    :class_name => 'Action',
+    :conditions => {:default_type => "gift"},
+    :autosave => true
 
-  accepts_nested_attributes_for :buy_action, :wear_action, :take_off_action, :gift_action, :actions
+  accepts_nested_attributes_for :buy_for_money_action, :buy_for_energy_action, :gift_action, :actions
 
   after_initialize :init_default_actions
 
-  has_one :item_type
+  belongs_to :item_type
+
+  def self.find_giftable
+    self.all(:conditions => {:item_types => {:giftable => true}}, :joins => :item_type)
+  end
+
   def dto
     ItemDTO.new self
   end
@@ -36,13 +35,13 @@ class Item < ActiveRecord::Base
   private #studio
 
   def init_default_actions
-		if new_record?
-			build_buy_action
-			build_wear_action
-			build_take_off_action
-			build_gift_action
-			buy_action.conditions.build
-		end
+    if new_record?
+      build_buy_for_money_action
+      build_buy_for_energy_action
+      build_gift_action
+      buy_for_money_action.conditions.build
+      buy_for_energy_action.conditions.build
+    end
   end
 
 end
