f239e44d24b8000455ce0b6479c638e815ef52d2
duke <duke@duke-vaio.(none)>
2011-09-22 20:16:49 +0400

    remove default from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Admin.find_or_create_by_email :email => "admin@foobar.com", :password => "p@ssw0rd"
 ItemType.find_or_create_by_name :name => "drinks",
 	:description => "Коктель",
 	:wearable => false,
