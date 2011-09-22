634a763b8b2501e1bbf779a5102009c77250cd20
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-22 17:11:07 +0400

    Message model scopes

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ActiveRecord::Base
-	has_one :source, :class_name => 'Character'
-	has_one :target, :class_name => 'Character'
+	has_one :source, :class_name => 'Character', :foreign_key => 'source_id'
+	has_one :target, :class_name => 'Character', :foreign_key => 'target_id'
+
+  scope :questions, where(:need_answer => true)
+  scope :rumors, where(:need_answer => false)
 
 	def dto
 		MessageDTO.new self
