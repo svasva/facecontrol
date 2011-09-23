0e3288ff07867701db139dafee79d4ae8f9d9cb0
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-23 18:08:30 +0400

    admin -> items form

--- app/admin/items.rb
+++ app/admin/items.rb
@@ -3,9 +3,23 @@ ActiveAdmin.register Item do
     column :name
     default_actions
   end
-collection_action :import_csv, :method => :post do
+	collection_action :import_csv, :method => :post do
     Item.parse_csv params[:import]
     redirect_to :action => :index, :notice => "CSV imported successfully!"
   end
 
+  form do |f|
+  	f.inputs 'Item' do
+	    f.input :name
+	    f.input :description
+	    f.input :picture_url
+	    f.input :ttl
+	    f.input :wear_factor
+	    f.input :glamour
+	    f.input :item_type,
+	    	:collection => ItemType.all.map {|i| ["#{i.name} (#{i.description})", i.id]}
+	    f.input :sex
+	  end
+    f.buttons
+  end
 end
