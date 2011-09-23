8480d728221b31b4a850d1c2d38147a07a439870
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-23 18:22:10 +0400

    move assets to http://facecontrol.erundook.net/assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Facecontrol::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://facecontrol.erundook.net/assets"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( active_admin.css active_admin.js )
