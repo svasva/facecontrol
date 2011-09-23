1bb968ed11bc8c5bae80d2d9a82acf2cd26c429b
duke <duke@duke-vaio.(none)>
2011-09-23 16:16:57 +0400

    actions edit page

--- app/admin/actions.rb
+++ app/admin/actions.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 ActiveAdmin.register Action do  
   belongs_to :place
   filter :name
@@ -6,7 +7,7 @@ ActiveAdmin.register Action do
     column :id
     column :name
     column :description
-	column :conditions do |action|
+  column :conditions do |action|
       div :class => "actions" do
         render "actions/condition_links", :action => action
       end
@@ -14,4 +15,31 @@ ActiveAdmin.register Action do
     column :created_at
     default_actions
   end
+
+  form do |f|
+    f.inputs "Details" do
+      f.input :name
+      f.input :description
+      f.input :action_group
+      f.input :contest_rating
+      f.input :need_target
+      f.input :delta_energy
+      f.input :delta_glory
+      f.input :delta_real_glory
+      f.input :delta_drive
+      f.input :delta_money
+      f.input :delta_wear
+    end
+    f.inputs "Timings" do
+      f.input :repeat
+      f.input :delay
+      f.input :ttl
+    end
+    f.inputs "Relations" do
+      f.input :disabler_action
+      f.input :children
+      f.input :parent
+    end    
+  end
+  
 end
