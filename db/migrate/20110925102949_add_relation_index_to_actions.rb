dba1c63cd28cd431fbac37c76b10ed921dd28a9b
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-25 15:23:52 +0400

    remove relation_index from actions & conditions

--- db/migrate/20110925102949_add_relation_index_to_actions.rb
+++
@@ -1,5 +0,0 @@
-class AddRelationIndexToActions < ActiveRecord::Migration
-  def change
-    add_column :actions, :delta_relation_index, :integer
-  end
-end
--- db/migrate/20110925103036_add_relation_index_to_conditions.rb
+++
@@ -1,5 +0,0 @@
-class AddRelationIndexToConditions < ActiveRecord::Migration
-  def change
-    add_column :conditions, :relation_index, :integer
-  end
-end
