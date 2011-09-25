e6bf8e9be5fdfe356174f83d8180f22c269d6072
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-25 21:20:39 +0400

    ClubDTO -> introduce :interval, :char_modifier

--- lib/models/place/club_dto.rb
+++ lib/models/place/club_dto.rb
@@ -2,6 +2,8 @@ class ClubDTO < PlaceDTO
 	attr_accessor :videos
 	attr_accessor :visitors
 	attr_accessor :bar_factor
+	attr_accessor :interval
+	attr_accessor :char_modifier
 
 	@videos, @visitors, @bar_factor = nil
 
@@ -10,6 +12,8 @@ class ClubDTO < PlaceDTO
 		@videos = place.video_urls.split("\n") unless place.video_urls.nil? or place.video_urls.empty?
 		@visitors = CharacterAction.where(:action_id => place.enter_action).where{character_id.not_eq char.id unless char.nil?}.order('id DESC').limit(50).map {|ca| ca.character.dto(char.id)}
 		@bar_factor = 1
+		@interval = place.stay_action.delay
+		@char_modifier = place.stay_action.dto
 	end
 end
 
