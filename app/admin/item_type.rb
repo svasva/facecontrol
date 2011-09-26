5d3dda471ffc724a9266d1671dd25f8c8fadbe00
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-26 23:13:26 +0400

    spaces -> tabs

--- app/admin/item_type.rb
+++ app/admin/item_type.rb
@@ -1,32 +1,32 @@
 ActiveAdmin.register ItemType, { :sort_order => :name_asc } do
-  filter :name
-  filter :description
+	filter :name
+	filter :description
 
-  index do
-  	column :name
-  	column :description
-  	column :wearable do |it|
-  		check_box_tag '', 0, it.wearable, {:disabled => true}
-  	end
-  	column :giftable do |it|
-  		check_box_tag '', 0, it.giftable, {:disabled => true}
-  	end
-  	column :usable do |it|
-  		check_box_tag '', 0, it.usable, {:disabled => true}
-  	end
-  	column :own_limit
-    column :items_count do |it|
-      it.items.count.to_s
-    end
-    default_actions
-  end
+	index do
+		column :name
+		column :description
+		column :wearable do |it|
+			check_box_tag '', 0, it.wearable, {:disabled => true}
+		end
+		column :giftable do |it|
+			check_box_tag '', 0, it.giftable, {:disabled => true}
+		end
+		column :usable do |it|
+			check_box_tag '', 0, it.usable, {:disabled => true}
+		end
+		column :own_limit
+		column :items_count do |it|
+			it.items.count.to_s
+		end
+		default_actions
+	end
 
-  collection_action :import_csv, :method => :post do
-    ItemType.parse_csv params[:import]
-    redirect_to :action => :index, :notice => "CSV imported successfully!"
-  end
+	collection_action :import_csv, :method => :post do
+		ItemType.parse_csv params[:import]
+		redirect_to :action => :index, :notice => "CSV imported successfully!"
+	end
 
-  sidebar 'CSV import' do
-    render "shared/upload", :action => :import_csv
-  end
+	sidebar 'CSV import' do
+		render "shared/upload", :action => :import_csv
+	end
 end
