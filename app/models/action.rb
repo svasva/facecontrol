fd64de034338501b20bec1e1be52d1f7944ead2d
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-28 20:10:13 +0400

    Action -> :disabler_action refactor

--- app/models/action.rb
+++ app/models/action.rb
@@ -9,14 +9,11 @@ class Action < ActiveRecord::Base
   	:class_name => 'Action',
   	:foreign_key => 'parent_id'
     
-  has_many :disabling_actions,
+  has_one :disabler_action,
   	:class_name => 'Action',
   	:foreign_key => 'disabler_action_id'
   	
   belongs_to :action_group
-  belongs_to :disabler_action,
-  	:class_name => 'Action',
-  	:foreign_key => 'disabler_action_id'
 
   scope :by_place, lambda {|place_id|
     where :subject_id => place_id, :subject_type => 'Place'
