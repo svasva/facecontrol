fb7e5d2bedc866e017a2d78d39e59fa607e8c2bd
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-28 14:30:07 +0400

    CharacterRelation scopes

--- app/models/character_relation.rb
+++ app/models/character_relation.rb
@@ -1,4 +1,7 @@
 class CharacterRelation < ActiveRecord::Base
 	belongs_to :character
 	belongs_to :target, :class_name => 'Character', :foreign_key => 'target_id'
+
+	scope :contacts, where(:friendship => true)
+	scope :contact_requests, where(:friendship => false, :friendship_request => true)
 end
