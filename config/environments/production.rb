2108c20726b7a3db1ed4a3227d0ed5092fa7f444
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-29 13:23:07 +0400

    asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Facecontrol::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://facecontrol.erundook.net"
+  config.action_controller.asset_host = "http://facecontrol.questa.pro"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( active_admin.css active_admin.js chosen.css chosen.jquery.js )
