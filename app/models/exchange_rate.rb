54ca9ea1a0c352ed004fab8248b0b9f126f68ba8
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-29 18:05:50 +0400

    ExchangeRate -> introduce :buy_action association

--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -1,2 +1,19 @@
 class ExchangeRate < ActiveRecord::Base
+  has_one :buy_action,
+		:as => :subject,
+		:class_name => 'Action',
+		:conditions => {:default_type => "buy_gold"},
+	  :autosave => true,
+	  :dependent => :destroy
+
+  def init_default_actions
+		if new_record?
+			build_buy_action
+			buy_action.conditions.build
+		end
+  end
+
+  def add_names_to_default_actions
+		buy_action.name = self.game_string
+  end
 end
