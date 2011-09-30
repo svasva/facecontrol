75282480afb60208efac7ac448e064ff11b7c5e8
duke <duke@duke-vaio.(none)>
2011-09-30 13:17:04 +0400

    item ttl import

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,12 +3,6 @@ require 'spec_helper'
 
 describe Item do
 
-  it "init with default actions" do
-    i = Item.new
-    i.buy_action.should_not == nil
-    i.use_action.should_not == nil
-    i.gift_action.should_not == nil
-  end
 
 
   context "parse drinks table" do
@@ -21,13 +15,22 @@ describe Item do
         0, 20, 5, 7, 8, 9, 200, "drink"]]
     end
 
+    let(:new_drink) {Item.last}
+    let(:new_drink2) {Item.first}
+    
+    it "init with default actions" do
+      Item.parse_drinks_table @table
+      new_drink.buy_action.should_not == nil
+      new_drink.use_action.should_not == nil
+      new_drink.gift_action.should_not == nil
+    end
+
     it "changes Items count" do
       expect do
         Item.parse_drinks_table @table
           end.to change {Item.count}.from(0).to(2)
     end
     
-    let(:new_drink) {Item.last}
 
     it "writes actions to new enteries" do
       Item.parse_drinks_table @table
@@ -56,7 +59,7 @@ describe Item do
       new_drink.buy_action.contest_rating.should == 200
     end
 
-    it "writes attributes to conditions of use_action" do
+    it "writes attributes to use_action of new enteries" do
       Item.parse_drinks_table @table
       new_drink.use_action.delta_energy.should == 7
       new_drink.use_action.delta_money.should == 0
@@ -65,14 +68,65 @@ describe Item do
       new_drink.use_action.contest_rating.should == 0
     end
 
-    it "writes attributes to conditions of gift_action" do
+    it "writes attributes to gift_action" do
       Item.parse_drinks_table @table
       new_drink.gift_action.delta_energy.should == 0
       new_drink.gift_action.delta_money.should == -20
       new_drink.gift_action.delta_drive.should == -5
       new_drink.gift_action.delta_glory.should == 9
       new_drink.gift_action.contest_rating.should == 200
+      new_drink.gift_action.need_target == true
     end
 
   end
+
+  context "parse gifts table" do
+    before(:all) do
+      Factory.create :gift_type
+      @table = [
+      [nil,'/game/Gifts/chisbuger.jpg', 'Дешевый подарок новичка', 'Описание',
+        60, nil, 60, 86400, 'gift'],
+      [nil,'/game/Gifts/brilliant.jpg', 'Мега подарок', 'Описание2',
+        nil, 100,10000, 31104000, 'gift']]
+    end
+
+    it "changes Items count" do
+      expect do
+        Item.parse_gifts_table @table
+          end.to change {Item.count}.from(0).to(2)
+    end
+    
+    let(:new_gift) {Item.last}
+
+    it "writes actions to new enteries" do
+      Item.parse_gifts_table @table
+      new_gift.actions.count.should == 1
+      new_gift.buy_action.should == nil
+      new_gift.gift_action.should_not == nil
+      new_gift.use_action.should == nil
+    end
+
+    it "writes attributes to new gifts" do
+      Item.parse_gifts_table @table    
+      new_gift.name.should == 'Мега подарок'
+      new_gift.description.should == 'Описание2'
+      new_gift.ttl.should == 31104000
+      new_gift.picture_url.should == '/game/Gifts/brilliant.jpg'
+      new_gift.item_type.name == 'gift'
+
+    end
+
+    it "writes attributes to gift_action of new enteries" do
+      Item.parse_gifts_table @table
+      new_gift.gift_action.delta_energy.should == 0
+      new_gift.gift_action.delta_money.should == -100
+      new_gift.gift_action.delta_drive.should == 0
+      new_gift.gift_action.delta_glory.should == 0
+      new_gift.gift_action.contest_rating.should == 10000
+      new_gift.gift_action.need_target.should == true
+    end
+
+  end
+
+
 end
