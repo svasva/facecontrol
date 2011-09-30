7f6ce5d8632a1136c3c2b72cd507ae4aa5c37c1b
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-30 14:15:11 +0400

    fix worker

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,4 @@
+require 'resque_scheduler'
 Dir[File.join(Rails.root, 'app', 'workers', '*.rb')].each { |file| require file }
 
 config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
