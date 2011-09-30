09d29c66cbcc161a5651456d0766379cc1546c97
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-30 19:31:03 +0400

    add charitem_id to giftdto

--- lib/models/item/gift_dto.rb
+++ lib/models/item/gift_dto.rb
@@ -1,11 +1,13 @@
 class GiftDTO < ItemDTO
 	attr_accessor :source
 	attr_accessor :remaining_ttl
+	attr_accessor :charitem_id
 
 	@source, @remaining_ttl = nil;
 
 	def initialize(char_item)
 		super(char_item.item)
+		@charitem_id = char_item.id
 		@source = char_item.source_character.dto
 		@remaining_ttl = char_item.remaining_ttl
 	end
