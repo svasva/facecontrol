4fce65288c34c5ea997b7f804be813ad6e4cbce9
Vitaly A. Sorokin <erundook@gmail.com>
2011-09-30 19:35:32 +0400

    ItemDTO gift drink properties fix

--- lib/models/item/gift_dto.rb
+++ lib/models/item/gift_dto.rb
@@ -7,6 +7,7 @@ class GiftDTO < ItemDTO
 
 	def initialize(char_item)
 		super(char_item.item)
+		@properties = char_item.item.use_action.dto if char_item.item.item_type.name == 'drink'
 		@charitem_id = char_item.id
 		@source = char_item.source_character.dto
 		@remaining_ttl = char_item.remaining_ttl
