112430feceebb3007031a4507e9ec2fe712a235f
duke <duke@duke-vaio.(none)>
2011-10-03 14:09:15 +0400

    added social_friends_count and places export

--- app/admin/places.rb
+++ app/admin/places.rb
@@ -13,9 +13,33 @@ ActiveAdmin.register Place, { :sort_order => :name_asc } do
     default_actions
   end
 
+  form do |f|
+    f.inputs 'Place' do
+      f.input :name
+      f.input :description
+      f.input :picture_url
+      f.input :map_x
+      f.input :map_y
+      f.input :video_urls
+      f.input :actions,
+        :collection => Action.all.map {|a| ["#{a.name} (#{a.description})", a.id]}
+    end
+    f.buttons
+  end
+
   collection_action :import_csv, :method => :post do
     Place.parse_csv params[:import]
     redirect_to :action => :index, :notice => "CSV imported successfully!"
+  end
+
+  collection_action :export, :method => :get do
+    respond_to do |f| 
+      f.csv {
+          send_data generate_csv, 
+            :type => 'text/csv; charset=utf-8; header=present', 
+            :disposition => "attachment; filename=places.csv"  
+          }
+    end
   end
 
   sidebar 'CSV import' do
