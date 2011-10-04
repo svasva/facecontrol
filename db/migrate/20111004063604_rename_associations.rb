ec83f70e34e7412555b3fa5d99556fdf908a47f3
duke <duke@duke-vaio.(none)>
2011-10-04 11:14:03 +0400

    sqlite index fix

--- db/migrate/20111004063604_rename_associations.rb
+++ db/migrate/20111004063604_rename_associations.rb
@@ -1,7 +1,9 @@
 class RenameAssociations < ActiveRecord::Migration
   def up
   	rename_column :conditions, :action_id, :game_action_id
+  	remove_index :character_actions, [:character_id, :action_id]
   	rename_column :character_actions, :action_id, :game_action_id
+  	add_index :character_actions, [:character_id, :game_action_id]
   end
 
   def down
