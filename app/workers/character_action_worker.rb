d96ad143e3ccd8216b2eeaa8fce3f71770a3d6c2
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-04 12:00:15 +0400

    CharacterActionWorker bugfixes

--- app/workers/character_action_worker.rb
+++ app/workers/character_action_worker.rb
@@ -9,15 +9,15 @@ class CharacterActionWorker
 		plong "= performing action #{ca.to_yaml} "
   	begin
 			if ca.status != 'canceled'
-				puts "PROCESSING! #{ca.action.name}"
+				puts "PROCESSING! #{ca.game_action.name}"
 				ca.process_action ? ca.done! : ca.failed!
 			else
-				puts "#{ca.action.name}(#{ca.action.id}) was canceled"
+				puts "#{ca.game_action.name}(#{ca.game_action.id}) was canceled"
 			end
 		rescue => e
 			puts e.inspect
 			ca.failed!
-			puts "#{ca.action.name}(#{ca.action.id}) FALIED!!!11 OMGWTF"
+			puts "#{ca.game_action.name}(#{ca.game_action.id}) FALIED!!!11 OMGWTF"
 		end
 	end
 end
