f14919699ca962f525667ba97ae0cbcbc57baade
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-05 12:56:01 +0400

    ItemDTO-> @gold_properties

--- lib/models/item/item_dto.rb
+++ lib/models/item/item_dto.rb
@@ -5,6 +5,7 @@ class ItemDTO
 	attr_accessor :image
 	attr_accessor :ttl
 	attr_accessor :properties
+	attr_accessor :gold_properties
 	attr_accessor :glamour
 	attr_accessor :wear_factor
 	attr_accessor :type
@@ -20,6 +21,7 @@ class ItemDTO
 		@wear_factor = item.wear_factor
 		@glamour = item.glamour
 		@properties = (item.item_type.name == 'gift') ? item.gift_action.dto : item.buy_action.dto
+		@gold_properties = (item.item_type.name == 'gift') ? item.gift_for_gold_action.dto : item.buy_for_gold_action.dto
 		@type = item.item_type.name
 	end
 end
