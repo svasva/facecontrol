27ff9efa8ac304f38f16787a5e5bbe182870c0bc
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-05 10:58:02 +0400

    more items bugfixing

--- app/admin/items.rb
+++ app/admin/items.rb
@@ -30,7 +30,7 @@ ActiveAdmin.register Item do
 	    f.input :glamour
 	    f.input :item_type,
 	    	:collection => ItemType.all.map {|i| ["#{i.name} (#{i.description})", i.id]}
-      f.input :actions,
+      f.input :game_actions,
         :collection => GameAction.all.map {|a| ["#{a.name} (#{a.description})", a.id]}
 	    f.input :sex
 	  end
