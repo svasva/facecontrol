0935fdc17dfadc56edb07969d714a09df0e0ee57
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-20 17:42:53 +0400

    contest stats -> column names

--- app/admin/character_action_groups.rb
+++ app/admin/character_action_groups.rb
@@ -1,16 +1,14 @@
 ActiveAdmin.register CharacterActionGroup, { :sort_order => :action_group_rating_desc } do
   menu :label => 'Contest stats'
-  
+
   filter :action_group
   filter :action_group_rating
   filter :created_at
 
   index do
-    column :action_group
+    column "Contest", :action_group
     column :character
-    column :action_group_rating
-    column :created_at
-
-    default_actions
+    column "rating", :action_group_rating
+    column "entered at", :created_at
   end
 end
