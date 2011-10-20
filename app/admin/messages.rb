62c8e3d88796ffc7355b3325aeeca8ce7a46aab7
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-20 14:31:12 +0400

    Add messages to admin

---
+++ app/admin/messages.rb
@@ -0,0 +1,14 @@
+ActiveAdmin.register Message do
+  scope :questions, :default => true
+  scope :rumors
+  index do
+    column :content
+    column :source
+    column :target
+    column :rating
+    column :anonymous do |msg|
+      check_box_tag '', 0, msg.anonymous, {:disabled => true}
+    end
+    default_actions
+  end
+end
